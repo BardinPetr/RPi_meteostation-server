92e1ae3d8db0a553fbb2ee02d94d6b25d27dea62
BardinPetr <bardin.petr@gmail.com>
2017-08-05 11:57:02 +0300

    Narodmon added

--- index.js
+++ index.js
@@ -87,7 +87,48 @@ function parseData(data) {
 
 var c = 0;
 
+/*
+NARODMON
+*/
+var net = require('net');
+var util = require('util');
 
+const DEVICE_MAC = "b827ebe8e8b9";
+const SENSOR_ID_1 = "b827ebe8e8b901";
+const SENSOR_ID_2 = "b827ebe8e8b902";
+const SENSOR_ID_3 = "b827ebe8e8b903";
+const SENSOR_ID_4 = "b827ebe8e8b904";
+const SENSOR_ID_5 = "b827ebe8e8b905";
+
+function uploadDataNM(data) {
+  var res;
+  return new Promise((resolve, reject) => {
+    try {
+      var client = new net.Socket();
+      client.connect(8283, 'narodmon.ru', function() {
+        client.write(util.format('#%s\n#%s#%d\n#%s#%d\n#%s#%d\n#%s#%d\n#%s#%d\n##', DEVICE_MAC, SENSOR_ID_1, data.temp, SENSOR_ID_2, data.humidity, SENSOR_ID_3, data.baro, SENSOR_ID_4, data.light, SENSOR_ID_5, data.dewpt));
+      });
+
+      client.on('data', function(_data) {
+        client.destroy();
+        res = _data.toString('utf-8');
+        res = res.substring(0, res.length - 1);
+      });
+
+      client.on('close', function() {
+        if (res != "OK")
+          reject(res);
+        else
+          resolve();
+      });
+    } catch (ex) {
+      reject(new Error('Parsing error: ' + ex));
+    }
+  });
+}
+/*
+ ******************************************************************
+ */
 
 function run() {
   requestData().then(data => {
@@ -96,6 +137,11 @@ function run() {
         log("[WU PACKETID: " + c + "]: OK")
       }).catch((error) => {
         log("[WU PACKETID: " + c + "]: ERR")
+      });
+      uploadDataNM(data.all).then(() => {
+        log("[NM PACKETID: " + c + "]: OK")
+      }).catch((error) => {
+        log("[NM PACKETID: " + c + "]: ERR: " + error)
       });
       c += 1;
     });
